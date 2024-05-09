29bb530408cdb8ccba6d8df9d1902b387e2aea22
ebsouza <ebsouza07@yahoo.com>
2024-05-09 14:00:01 -0300

    add mongodb connection

---
+++ mongodb/README.md
@@ -0,0 +1,15 @@
+## MongoDB connection
+
+
+```
+go mod init github.com/ebsouza/go-code/mongodb
+
+got get go.mongodb.org/mongo-driver/mongo@v1.15.0
+
+go get github.com/joho/godotenv@v1.5.1
+```
+
+
+### To see more about operations on MongoDB
+
+- [Quick Reference](https://www.mongodb.com/docs/drivers/go/current/quick-reference/)
---
+++ mongodb/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("Set your 'MONGODB_URI' environment variable. ")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+
+	client, err := mongo.Connect(ctx, options.Client().
+		ApplyURI(uri))
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		cancel()
+		if err := client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatalf("ping mongodb error :%v", err)
+		return
+	}
+	fmt.Println("ping success")
+}
