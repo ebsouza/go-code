5542fe0e5e854aa4fa7df14f9daeec5a85c64ee2
ebsouza <ebsouza07@yahoo.com>
2024-08-30 16:53:13 -0300

    goci: add git push process

--- goci/main.go
+++ goci/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 type executer interface {
@@ -16,10 +17,11 @@ func run(proj string, out io.Writer) error {
 		fmt.Errorf("Project diretory is required")
 	}
 
-	pipeline := make([]executer, 3)
+	pipeline := make([]executer, 4)
 	pipeline[0] = newStep("go build", "go", "Go Build: SUCCESS", proj, []string{"build", ".", "errors"})
 	pipeline[1] = newStep("go test", "go", "Go Test: SUCCESS", proj, []string{"test", "-v"})
 	pipeline[2] = newExceptionStep("go fmt", "gofmt", "Gofmt: SUCCESS", proj, []string{"-l", "."})
+	pipeline[3] = newTimeoutStep("git push", "git", "Git Push: SUCCESS", proj, []string{"push", "origin", "master"}, 10*time.Second)
 
 	for _, s := range pipeline {
 		msg, err := s.execute()
