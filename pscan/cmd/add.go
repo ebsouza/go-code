e44b278f14bf63b1027e7afe58b247a2d53f4d63
ebsouza <ebsouza07@yahoo.com>
2024-09-11 08:27:56 -0300

    pscan: add command

---
+++ pscan/cmd/add.go
@@ -0,0 +1,52 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/ebsouza/go-code/pscan/scan"
+	"github.com/spf13/cobra"
+)
+
+// addCmd represents the add command
+var addCmd = &cobra.Command{
+	Use:          "add <host1>...<host2>",
+	Short:        "Add new host(s) to list",
+	Args:         cobra.MinimumNArgs(1),
+	Aliases:      []string{"a"},
+	SilenceUsage: true,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		hostsFile, err := cmd.Flags().GetString("hosts-file")
+		if err != nil {
+			return err
+		}
+
+		return addAction(os.Stdout, hostsFile, args)
+	},
+}
+
+func init() {
+	hostsCmd.AddCommand(addCmd)
+}
+
+func addAction(out io.Writer, hostsFile string, args []string) error {
+	hl := scan.HostsList{}
+
+	if err := hl.Load(hostsFile); err != nil {
+		return err
+	}
+
+	for _, h := range args {
+		if err := hl.Add(h); err != nil {
+			return err
+		}
+
+		fmt.Fprintln(out, "Added host:", h)
+	}
+
+	return hl.Save(hostsFile)
+}
